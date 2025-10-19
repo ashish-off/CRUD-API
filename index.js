215c1286d633f97c4121abfc71d75963674459da
Ashish <ashishsaud498@gmail.com>
2025-10-19 14:37:06 +0545

    add get and post products

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/products.model.js')
 const app = express();
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.status(200).json({ message: 'Welcome to the node API' });
+})
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({ message : error.message})
+    }
+    
+})
+
+app.get('/api/products' ,async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message : error.message});
+        
+    }
 })
 
 mongoose.connect('mongodb+srv://ashishsaud498_db_user:wyRUArsqXznQHyKz@backenddb.v6eba3w.mongodb.net/Node-API').then(() => {
