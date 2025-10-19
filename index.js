b147aba0431f95b3ecee675e423bff4982f705ef
Ashish <ashishsaud498@gmail.com>
2025-10-19 13:07:04 +0545

    mongoDB atlas connection

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000');
-    
-})
+mongoose.connect('mongodb+srv://ashishsaud498_db_user:wyRUArsqXznQHyKz@backenddb.v6eba3w.mongodb.net/Node-API').then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+        console.log('server is running at http://localhost:3000');
+        
+    })
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
