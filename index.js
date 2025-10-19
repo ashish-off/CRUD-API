e5ee138a0cf7d5be7edaed0ff5b1cc71f7f69392
Ashish <ashishsaud498@gmail.com>
2025-10-19 15:10:15 +0545

    add get item by id

--- index.js
+++ index.js
@@ -9,23 +9,48 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the node API' });
 })
 
+// CREATE a product 
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
     } catch (error) {
-        res.status(500).json({ message : error.message})
+        res.status(500).json({ message: error.message })
     }
-    
+
 })
 
-app.get('/api/products' ,async (req, res) => {
+// READ all items
+app.get('/api/products', async (req, res) => {
     try {
         const products = await Product.find();
         res.status(200).json(products);
     } catch (error) {
-        res.status(500).json({message : error.message});
-        
+        res.status(500).json({ message: error.message });
+
+    }
+})
+
+
+// READ by id
+app.get('/api/product/:id', async (req, res) => {
+    const { id } = req.params;
+
+    // not a 24-character hexadecimal string
+   if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid product id format' });
+    }
+
+    try {
+        const product = await Product.findById(id);
+        // upon returning null
+        if (!product) {
+            return res.status(404).json({ message: `Product with id ${id} not found` });
+        } else {
+            res.status(200).json(product);
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
@@ -33,7 +58,7 @@ mongoose.connect('mongodb+srv://ashishsaud498_db_user:wyRUArsqXznQHyKz@backenddb
     console.log('Connected to MongoDB');
     app.listen(3000, () => {
         console.log('server is running at http://localhost:3000');
-        
+
     })
 }).catch((error) => {
     console.error('MongoDB connection error:', error);
